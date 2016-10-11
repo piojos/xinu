7b48cd6d0208223acc6d764c2c55b8ae70af89c0
raidho-mx <miranda@raidho.mx>
2016-10-11 17:08:39 -0500

    first draft

--- js/magic.js
+++ js/magic.js
@@ -1,26 +1,31 @@
 var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("#polina button");
+// var menuButtons = document.getElementsByClassName("nav a");
+// var pauseButton = document.querySelector("#polina button");
 
 function vidFade() {
 	vid.classList.add("stopfade");
-}
+};
 
-vid.addEventListener('ended', function()
-{
-	// only functional if "loop" is removed
-	vid.pause();
-	// to capture IE10
-	vidFade();
-});
+// menuButtons.addEventListener('click', function(){
+// 	console.log('EE');
+// });
 
 
-pauseButton.addEventListener("click", function() {
-	vid.classList.toggle("stopfade");
-	if (vid.paused) {
-		vid.play();
-		pauseButton.innerHTML = "Pause";
-	} else {
-		vid.pause();
-		pauseButton.innerHTML = "Paused";
-	}
-})
+// vid.addEventListener('ended', function(){
+// 	vid.pause();
+// 	vidFade();
+// });
+
+
+// pauseButton.addEventListener("click", function() {
+// 	vid.classList.toggle("stopfade");
+// 	if (vid.paused) {
+// 		vid.play();
+// 		pauseButton.innerHTML = "Pause";
+// 	} else {
+// 		vid.pause();
+// 		pauseButton.innerHTML = "Paused";
+// 	}
+// })
+
+	console.log('0');
