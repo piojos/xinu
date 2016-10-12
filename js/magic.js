756014485f0392205806b705a63340819f7f7d8f
raidho-mx <miranda@raidho.mx>
2016-10-12 09:38:44 -0500

    Language switcher

--- js/magic.js
+++ js/magic.js
@@ -1,15 +1,30 @@
 // var vid = document.getElementById("bgvid");
 var speed = 400;
+var audio_file = new Audio('assets/background_music.mp3');
 
 $('nav a.inner').click(function(){
 	var who = this.getAttribute('href');
 	$('.content, #menu, .watermark img').fadeOut(speed);
+	$('.overlay').fadeIn(speed);
 	$(who).delay(speed).fadeIn(speed);
 	$('.watermark > a').delay(speed).fadeIn(speed);
 });
 
 $('.watermark > a').click(function() {
 	$(this).fadeOut(speed);
-	$('.content').fadeOut(speed);
+	$('.content, .overlay').fadeOut(speed);
 	$('#menu, .watermark img').delay(speed).fadeIn(speed);
 });
+
+$('nav a.lang').click(function(){
+	$(this).parent().fadeOut(speed);
+	$(this).parent().siblings().delay(speed).fadeIn(speed);
+});
+
+audio_file.addEventListener('timeupdate', function(){
+	console.log('ee');
+	var buffer = .44
+	if(this.currentTime > this.duration - buffer){
+	this.currentTime = 0
+	this.play()
+}}, false);
