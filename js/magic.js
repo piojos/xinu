6c0959ccc23a975a2f982ebd818a8a0e96590723
raidho-mx <miranda@raidho.mx>
2016-10-19 11:35:13 -0500

    Discard notes

--- js/magic.js
+++ js/magic.js
@@ -1,20 +1,31 @@
-// var vid = document.getElementById("bgvid");
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	$(document).ready(function($) {
+		$('body').prepend("<div class='slider'><ul><li style='background:url(assets/sl/1.jpg);'></li><li style='background:url(assets/sl/2.jpg);'></li><li style='background:url(assets/sl/3.jpg);'></li><li style='background:url(assets/sl/4.jpg);'></li><li style='background:url(assets/sl/5.jpg);'></li><li style='background:url(assets/sl/6.jpg);'></li></ul></div>");
+		$('.slider').unslider({
+			animation: 'fade',
+			autoplay: true,
+			arrows: false,
+			speed: 1000,
+			delay: 2000,
+			nav: false
+		});
+	});
+} else {
+	$('body').prepend("<video poster='assets/background_fallback.jpg' id='bgvid' playsinline autoplay muted loop ><source src='assets/background_video.webm' type='video/webm'><source src='assets/background_video.mp4' type='video/mp4'></video>");
+}
+
+
 var speed = 400;
 var audio_file = new Audio('assets/background_music.mp3');
 
-// Nice other method: detects browser language preferences
-// var language = window.navigator.userLanguage || window.navigator.language;
-// alert(language);
 
 $.get("http://ipinfo.io", function (response) {
 	var country = response.country;
 	var spanish = ['ES', 'AR', 'BO', 'CL', 'CO', 'CR', 'CU', 'DO', 'EC', 'SV', 'GT', 'HN', 'MX', 'NI', 'PN', 'PA', 'PE', 'PR', 'UY', 'VE'];
 	if( $.inArray(country, spanish) != -1){
-		// console.log('español');
 		$('nav .es').fadeIn('slow');
 		$('.watermark').delay(speed).fadeIn(speed);
 	} else {
-		// console.log('english');
 		$('nav .en').fadeIn('slow');
 		$('.watermark').delay(speed).fadeIn(speed);
 	}
@@ -38,29 +49,3 @@ $('nav a.lang').click(function(){
 	$(this).parent().fadeOut(speed);
 	$(this).parent().siblings().delay(speed).fadeIn(speed);
 });
-
-// audio_file.addEventListener('timeupdate', function(){
-// 	console.log('ee');
-// 	var buffer = .44
-// 	if(this.currentTime > this.duration - buffer){
-// 	this.currentTime = 0
-// 	this.play()
-// }}, false);
-
-
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	$(document).ready(function($) {
-		$('body').prepend("<div class='slider'><ul><li style='background:url(assets/sl/1.jpg);'></li><li style='background:url(assets/sl/2.jpg);'></li><li style='background:url(assets/sl/3.jpg);'></li><li style='background:url(assets/sl/4.jpg);'></li><li style='background:url(assets/sl/5.jpg);'></li><li style='background:url(assets/sl/6.jpg);'></li></ul></div>");
-		$('.slider').unslider({
-			animation: 'fade',
-			autoplay: true,
-			arrows: false,
-			speed: 1000,
-			delay: 2000,
-			nav: false
-		});
-	});
-} else {
-	$('body').prepend("<video poster='assets/background_fallback.jpg' id='bgvid' playsinline autoplay muted loop ><source src='assets/background_video.webm' type='video/webm'><source src='assets/background_video.mp4' type='video/mp4'></video>");
-}
